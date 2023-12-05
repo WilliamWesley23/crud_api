886b1a7680dc04990c5b14bf104fcd82fbbbfffe
Caua Willian <00019536@fepi.edu.br>
2023-12-05 16:40:25 -0300

    atualização Cauã Willian

--- src/main/java/br/fepi/apicrud/repository/EmpresasRepository.java
+++ src/main/java/br/fepi/apicrud/repository/EmpresasRepository.java
@@ -1,8 +1,14 @@
 package br.fepi.apicrud.repository;
 
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.fepi.apicrud.domain.Empresa;
+
+
+
 
 public interface EmpresasRepository extends JpaRepository<Empresa, Long>{ 
 
